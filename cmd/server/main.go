02a2f84c722434105ddf02feb9257b29a213e5c4
Vorobjev Valery <vorobjevvalera@gmail.com>
2025-07-29 21:01:11 +0300

    Update VSCode settings and clean up main.go
    Enabled server, auto-formatting, and import actions in VSCode settings for. Additionally, removed unused imports and redundant code from main.go for improved code cleanliness.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,15 +2,8 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/jackc/pgx/v5/pgtype"
-	"github.com/valeravorobjev/lemming-trip/internal/sqlc/db"
 )
 
 func main() {
 	fmt.Println("Hello")
-
-	a := db.Author{ID: 1, Name: "Hello", Bio: pgtype.Text{String: "Hello", Valid: true}}
-
-	fmt.Printf("Id = %d, Name = %s\n", a.ID, a.Name)
 }
